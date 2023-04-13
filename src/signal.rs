a5c59d209efcca8c9f2a9756d8e6cce2db7160ef
Benjamin Lefebvre <benji.lef76@gmail.com>
2023-04-14 01:18:32 +0200

    try methods

--- src/signal.rs
+++ src/signal.rs
@@ -4,34 +4,59 @@ use alloc::boxed::Box;
 use alloc::rc::Rc;
 use alloc::vec::Vec;
 
-fn fn_id<T>(f: &Box<dyn FnMut(&T)>) -> usize {
+type Notifier<T> = Box<dyn FnMut(&T)>;
+
+#[inline(always)]
+fn id_of<T>(f: &Notifier<T>) -> usize {
     f as *const dyn FnMut(&T) as *const () as usize
 }
+
+#[derive(Debug)]
+pub struct SignalUpdatingError;
 
 pub trait Subscribable<T> {
     fn subscribe(&self, notify: impl FnMut(&T) + 'static) -> Unsubscriber;
 }
 
-struct SignalInternal<T> {
+struct RawStore<T> {
     data: T,
-    subscribers: Vec<Box<dyn FnMut(&T)>>,
+    subscribers: Vec<Notifier<T>>,
+    updating: bool,
 }
 
-impl<T> SignalInternal<T> {
-    fn find(&self, id: usize) -> Result<usize, usize> {
-        self.subscribers.binary_search_by_key(&id, |f| fn_id(f))
+impl<T> RawStore<T> {
+    fn new(data: T) -> Self {
+        Self {
+            data,
+            subscribers: Vec::new(),
+            updating: false,
+        }
     }
 
-    fn mutate(&mut self, mutater: impl FnOnce(&mut T)) {
+    fn find(&self, id: usize) -> Result<usize, usize> {
+        self.subscribers.binary_search_by_key(&id, id_of)
+    }
+
+    fn try_mutate(&mut self, mutater: impl FnOnce(&mut T)) -> Result<(), SignalUpdatingError> {
+        if self.updating {
+            return Err(SignalUpdatingError);
+        }
+
+        self.updating = true;
+
         mutater(&mut self.data);
         
         for subscriber in &mut self.subscribers {
             subscriber(&self.data);
         }
+
+        self.updating = false;
+
+        Ok(())
     }
 
     fn subscribe(&mut self, mut subscriber: Box<dyn FnMut(&T)>) -> usize {
-        let id = fn_id(&subscriber);
+        let id = id_of(&subscriber);
         let index = self.find(id).unwrap_err();
         subscriber(&self.data);
         self.subscribers.insert(index, subscriber);
@@ -45,36 +70,56 @@ impl<T> SignalInternal<T> {
 }
 
 #[repr(transparent)]
-pub struct Signal<T>(Rc<UnsafeCell<SignalInternal<T>>>);
+pub struct Signal<T>(Rc<UnsafeCell<RawStore<T>>>);
 
 impl<T> Signal<T> {
     #[inline(always)]
-    unsafe fn internal(&self) -> &mut SignalInternal<T> {
+    unsafe fn internal(&self) -> &mut RawStore<T> {
         &mut *self.0.get()
     }
 
     pub fn new(data: T) -> Self {
-        let internal = SignalInternal {
-            data,
-            subscribers: Vec::new(),
-        };
-
+        let internal = RawStore::new(data);
         Self(Rc::new(UnsafeCell::new(internal)))
     }
 
-    pub fn mutate(&self, mutater: impl FnOnce(&mut T)) {
+    pub fn get(&self) -> T 
+    where
+        T: Clone 
+    {
         let internal = unsafe { self.internal() };
-        internal.mutate(mutater);
+        internal.data.clone()
+    }
+
+    #[inline(always)]
+    pub fn try_mutate(&self, mutater: impl FnOnce(&mut T)) -> Result<(), SignalUpdatingError> {
+        let internal = unsafe { self.internal() };
+        internal.try_mutate(mutater)
+    }
+
+    #[inline(always)]
+    pub fn mutate(&self, mutater: impl FnOnce(&mut T)) {
+        self.try_mutate(mutater).unwrap();
+    }
+
+    #[inline(always)]
+    pub fn try_update(&self, updater: impl FnOnce(&T) -> T) -> Result<(), SignalUpdatingError> {
+        self.try_mutate(|data| *data = updater(data))
     }
 
     #[inline(always)]
     pub fn update(&self, updater: impl FnOnce(&T) -> T) {
-        self.mutate(|data| *data = updater(data))
+        self.try_update(updater).unwrap()
+    }
+
+    #[inline(always)]
+    pub fn try_set(&self, data: T) -> Result<(), SignalUpdatingError> {
+        self.try_mutate(move |old| *old = data)
     }
 
     #[inline(always)]
     pub fn set(&self, data: T) {
-        self.mutate(move |old| *old = data)
+        self.try_set(data).unwrap();
     }
 
     /* map, filter, reduce, ... */
@@ -116,6 +161,7 @@ impl Unsubscriber {
     }
 }
 
+#[must_use]
 #[repr(transparent)]
 pub struct DropUnsubscriber(Unsubscriber);
 
