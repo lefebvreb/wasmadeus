fe0f538f8953d01ff69e54fe334438f878c52de7
Benjamin Lefebvre <benji.lef76@gmail.com>
2023-05-23 23:34:01 +0200

    thinking about the future

--- src/attribute.rs
+++ src/attribute.rs
@@ -1,22 +1,22 @@
-use web_sys::HtmlElement;
+use web_sys::Element;
 
 use crate::signal::Value;
 use crate::utils::for_all_tuples;
 
 pub trait Attribute {
-    fn apply_to(self, element: &HtmlElement);
+    fn apply_to(self, element: &Element);
 }
 
 impl Attribute for () {
     #[inline]
-    fn apply_to(self, _: &HtmlElement) {}
+    fn apply_to(self, _: &Element) {}
 }
 
 macro_rules! impl_tuple_attribute {
     ($($name: ident)*) => {
         impl<$($name: Attribute,)*> Attribute for ($($name,)*) {
             #[allow(non_snake_case)]
-            fn apply_to(self, element: &HtmlElement) {
+            fn apply_to(self, element: &Element) {
                 let ($($name,)*) = self;
                 $($name.apply_to(element);)*
             }
@@ -36,7 +36,7 @@ where
     T::Item: AsRef<str>,
 {
     #[inline]
-    fn apply_to(self, element: &HtmlElement) {
+    fn apply_to(self, element: &Element) {
         let element = element.clone();
         self.0.for_each_forever(move |value| {
             element.set_class_name(value.as_ref());
--- src/component.rs
+++ src/component.rs
@@ -1,5 +1,5 @@
-use web_sys::HtmlElement;
+use web_sys::Element;
 
 pub trait Component {
-    fn append_to(self, parent: HtmlElement);
+    fn append_to(self, parent: Element);
 }
